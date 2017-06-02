806dd6fcb11fee2009f4e9133032f8dc37118c8c
Jason Yang <jyang@vonw8034d5qagu.vonage.net>
2017-06-02 16:33:03 -0400

    create webhook

--- app.js
+++ app.js
@@ -17,20 +17,78 @@ app.get('/', function (req, res) {
 
 app.post('/fulfillment', function (req, res) {
     console.log('app.post/fulfillment. body=', JSON.stringify(req.body));
-    var response = {
-        speech: "Today in Boston: Fair, the temperature is 37 F",
-        source: "apiai-weather-webhook-sample",
-        displayText: "Today in Boston: Fair, the temperature is 37 F"
-    }
+    var streetArray = req.body.result.parameters['street-address'];
+    var stations;
     rp(STATION_INFO_URI)
         .then(function (data) {
-            console.log(data);
+            var obj = JSON.parse(data);
+            stations = get_station_by_name(streetArray, obj.data['stations']);
+        })
+        .then(function () {
+            return rp(STATION_STATUS_URI);
+        })
+        .then(function (data) {
+            var obj = JSON.parse(data);
+            var ids = stations.map(function (station) {
+                return station['station_id'];
+            });
+            var all_status = get_status_by_id(ids, obj.data['stations']);
+            var message = process_status_message(stations, all_status);
+
+            var response = {
+                speech: message,
+                source: "apiai-citibike",
+                displayText: message
+            }
+            console.log(response);
+            res.json(response);
         })
         .catch(function (err) {
             console.log(err);
         })
-    res.json(response);
+
 });
 
 var port = process.env.port || process.env.PORT || 3000;
 app.listen(port, () => { console.log('Chatbot application Running on port .' + port) });
+
+function get_station_by_name(streetArray, stations) {
+    return stations.filter(function(station) {
+        return test(streetArray, station);
+    });
+}
+
+function get_status_by_id(ids, allStatus) {
+    return allStatus.filter(function (statusObj) {
+        if(ids.indexOf(statusObj['station_id']) !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
+
+function test(streetArray, station) {
+    var newArray = streetArray.map(function (street) {
+        var news = street.replace(/street/gi, 'st').replace(/avenue/gi, 'ave');
+        return news;
+    });
+
+    for(var i = 0; i < newArray.length; i ++) {
+        if(station['name'].toLowerCase().indexOf(newArray[i].toLowerCase()) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function process_status_message(stations, all_status) {
+    var res = '';
+    all_status.forEach(function (status, index) {
+        res = res + 'Station Name: ' + stations[index]['name'] + '\n';
+        res = res + 'Available Bikes: ' + status['num_bikes_available'] + '\n';
+        res = res + 'Available Docks: ' + status['num_docks_available'] + '\n';
+        res = res + '\n';
+    });
+    return res;
+}

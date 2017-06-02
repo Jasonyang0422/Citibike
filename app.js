1b69a0ceb6856acdca451b826e71810186664339
Jason Yang <jyang@vonw8034d5qagu.vonage.net>
2017-06-02 15:03:18 -0400

    initialize

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const STATION_INFO_URI = "https://gbfs.citibikenyc.com/gbfs/en/station_information.json";
+const STATION_STATUS_URI = "https://gbfs.citibikenyc.com/gbfs/en/station_status.json";
+
 let express = require('express');
 let bodyParser = require('body-parser');
+let rp = require('request-promise');
 
 let app = express();
 app.use(bodyParser.json({ type: 'application/json' }));
@@ -13,12 +17,19 @@ app.get('/', function (req, res) {
 
 app.post('/fulfillment', function (req, res) {
     console.log('app.post/fulfillment. body=', JSON.stringify(req.body));
-    var json = {
+    var response = {
         speech: "Today in Boston: Fair, the temperature is 37 F",
         source: "apiai-weather-webhook-sample",
         displayText: "Today in Boston: Fair, the temperature is 37 F"
     }
-    res.json(json);
+    rp(STATION_INFO_URI)
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+    res.json(response);
 });
 
 var port = process.env.port || process.env.PORT || 3000;
